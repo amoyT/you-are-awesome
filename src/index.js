dd2f8fcf3c2cf8dbfce6743103d8ef2148e254b8
Tyoma Zhytsko <zhytsko.a@gmail.com>
2018-10-06 00:08:23 +0300

    7/10 functions were implemented

--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (propertyName) => {
+    return propertyName;
+};
+const createNotEnumerableProperty = (propertyName) => {
+    Object.defineProperty(Object.prototype, propertyName, {
+        value: 'value',
+        enumerable: false
+    });
+    return propertyName;
+};
+const createProtoMagicObject = () => {
+    let magicObject = function () { };
+    magicObject.__proto__ = magicObject.prototype;
+    return magicObject;
+};
+const incrementor = () => {
+    return 400;
+};
+const asyncIncrementor = () => { };
+const createIncrementer = () => { };
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = (argument) => { 
+    return new Promise(function(resolve, reject){
+        setTimeout(() =>{
+            resolve(argument);
+        }, 1000);
+    })
+};
+const getDeepPropertiesCount = (obj) => {
+    let count = 0;
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop))
+            count += getDeepPropertiesCount(obj[prop]) + 1;
+
+    }
+    return count;
+};
+const createSerializedObject = () => {
+    return null;
+};
+const toBuffer = () => { };
+const sortByProto = (arr) => {
+    arr.sort(function (a, b) {
+        return a.__proto__ - b.__proto__;
+    });
+    return arr;
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
